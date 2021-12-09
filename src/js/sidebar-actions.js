205b0d74cdb8a16cb94d1b298ebf0529cdcc4152
Quebyn <quebynliga@gmail.com>
2021-12-09 00:07:00 -0700

    Fix para addEventListener null en generarReporte

--- src/js/sidebar-actions.js
+++ src/js/sidebar-actions.js
@@ -1,9 +1,11 @@
 const generarReporte = document.querySelector('.generarReporte');
 const sideContainer = document.querySelector('.sideContainer');
 
-generarReporte.addEventListener('click', () => {
-    sideContainer.innerHTML = '<p class="title2">Generar Reporte</p> <hr class="solid"></hr> <form> <p class="subtitle">Título</p> <input class="textBox" placeholder="Escriba un título"></input> <p class="subtitle">Descripción</p> <input class="textBox textBox-desc" placeholder="Escriba una descripción"></input> <p class="subtitle">Adjuntar Foto</p> <input class="textBox" placeholder="Adjunte una foto (Opcional)"></input> <p class="subtitle">Ubicación</p> <input class="textBox" placeholder="Seleccione una ubicación"></input> <button class="confirmarBtn">Confirmar</button> </form>'
-});
+if(generarReporte){
+    generarReporte.addEventListener('click', () => {
+        sideContainer.innerHTML = '<p class="title2">Generar Reporte</p> <hr class="solid"></hr> <form> <p class="subtitle">Título</p> <input class="textBox" placeholder="Escriba un título"></input> <p class="subtitle">Descripción</p> <input class="textBox textBox-desc" placeholder="Escriba una descripción"></input> <p class="subtitle">Adjuntar Foto</p> <input class="textBox" placeholder="Adjunte una foto (Opcional)"></input> <p class="subtitle">Ubicación</p> <input class="textBox" placeholder="Seleccione una ubicación"></input> <button class="confirmarBtn">Confirmar</button> </form>'
+    });
+}
 
 
 
