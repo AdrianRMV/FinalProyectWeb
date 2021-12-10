9d358dcbc06664647e3228f64fd5eb145cb7cf9d
Adrian <adrianthelawx@gmail.com>
2021-12-09 22:25:49 -0700

    Crea etiquetas apartir de la informacion traida de la data

--- src/js/retrieve.js
+++ src/js/retrieve.js
@@ -1,6 +1,7 @@
 // const $reportTitle = document.querySelector('#reportTitle');
 // const $desc = document.querySelector('#desc');
 const $getReporte = document.querySelector('.verReporte');
+const  $contenedorDerecho = document.querySelector('.sideContainer');
 
 if($getReporte){
       $getReporte.addEventListener("click", (event)=>{
@@ -27,6 +28,34 @@ if($getReporte){
                         console.log(response);
                         console.log(response.data.length);
                         console.log(response.data[0].titulo);
+
+                        for(let i = 0; i < response.data.length; i++){
+                              //crear elementos dentro del contenedor derecho
+                              let $div = document.createElement('div');
+                                    $div.classList.add('reporte-contenedor'); //CLASE PARA EL DIV (reporte)
+
+                                    let $titulo = document.createElement('h3');
+                                    $titulo.classList.add('reporte-titulo'); //CLASE PARA EL TITULO (reporte-titulo)
+                                    $titulo.innerHTML = response.data[i].titulo;
+                                    $div.appendChild($titulo);
+
+
+                                    let $descripcion = document.createElement('p');
+                                    $descripcion.classList.add('reporte-descripcion'); //CLASE PARA LA DESCRIPCION (reporte-descripcion)
+                                    $descripcion.innerHTML = response.data[i].descripcion;
+                                    $div.appendChild($descripcion);
+
+                                    // crea un boton nuevo y agregalo al DIV
+                                    let $boton = document.createElement('button');
+                                    $boton.classList.add('reporte-boton'); //CLASE PARA EL BOTON (reporte-boton)
+                                    $boton.innerHTML = "Ver Reporte";
+                                    $div.appendChild($boton);
+                                    
+                              $contenedorDerecho.appendChild($div);
+                        }
+
+
+
                         alert("Me la pelo el reporte");
                         console.log('que pro');
                   } else {
