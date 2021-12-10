7434967722dce21daebe52be57efd781914186fa
Adrian <adrianthelawx@gmail.com>
2021-12-10 10:35:10 -0700

    Falta terminar ver reportes, no localiza boton

--- src/js/revision-reportes.js
+++ src/js/revision-reportes.js
@@ -1,13 +1,13 @@
-const  btnRevisar = document.querySelector('.reporte-boton-1');
+const  $btnRevisar = document.querySelectorAll('#reporte-boton')[0];
 // const ubicacion = document.getElementsByClassName('reporte-ubicacion-1');
 
 // cambiar la variable ubicacion a entero
 // ubicacion = parseInt(ubicacion.value);
 // console.log(ubicacion);
-
-// if (btnRevisar) {
-    btnRevisar.addEventListener("click", (event) =>{
+console.log("entre al script");
+if ($btnRevisar) {
+    $btnRevisar.addEventListener("click", (event) =>{
         event.preventDefault();
         console.log("Entre");
     });
-// }
+}
