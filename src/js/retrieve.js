073782b97ae4934353bafc5d49f5465f769f2562
Adrian <adrianthelawx@gmail.com>
2021-12-10 13:13:46 -0700

    Ver reportes a la mitad

--- src/js/retrieve.js
+++ src/js/retrieve.js
@@ -1,31 +1,31 @@
 // const $reportTitle = document.querySelector('#reportTitle');
 // const $desc = document.querySelector('#desc');
 const $getReporte = document.querySelector('.verReporte');
-const  $contenedorDerecho = document.querySelector('.sideContainer');
+const $contenedorDerecho = document.querySelector('.sideContainer');
 
-if($getReporte){
-      $getReporte.addEventListener("click", (event)=>{
+if ($getReporte) {
+      $getReporte.addEventListener("click", (event) => {
             event.preventDefault();
-            const  data = new FormData();
-        
-            axios({
-                  method: 'POST',
-                  url: "/api/user/retrieve.php",
-                  data,
-                  headers: {
-                        "Content-Type": "multipart/form-data"
-                  },
-            })
-            .then((response)=>{
-                  if (response.status === 200){
-                        
-                        console.log(response);
-                        console.log(response.data.length);
-                        console.log(response.data[0].titulo);
+            const data = new FormData();
 
-                        for(let i = 0; i < response.data.length; i++){
-                              //crear elementos dentro del contenedor derecho
-                              let $div = document.createElement('div');
+            axios({
+                        method: 'POST',
+                        url: "/api/user/retrieve.php",
+                        data,
+                        headers: {
+                              "Content-Type": "multipart/form-data"
+                        },
+                  })
+                  .then((response) => {
+                        if (response.status === 200) {
+
+                              console.log(response);
+                              console.log(response.data.length);
+                              console.log(response.data[0].titulo);
+
+                              for (let i = 0; i < response.data.length; i++) {
+                                    //crear elementos dentro del contenedor derecho
+                                    let $div = document.createElement('div');
                                     $div.classList.add(`reporte-contenedor`); //CLASE PARA EL DIV (reporte)
 
                                     let $innerDiv = document.createElement('div');
@@ -38,8 +38,8 @@ if($getReporte){
                                     $innerDiv.appendChild($titulo);
 
                                     let $estatus = document.createElement('img');
-                                    $estatus.classList.add('status-img');//CLASE PARA EL ESTATUS (status-img)
-                                    $estatus.src = '/src/images/'+response.data[i].estatus+'.png';
+                                    $estatus.classList.add('status-img'); //CLASE PARA EL ESTATUS (status-img)
+                                    $estatus.src = '/src/images/' + response.data[i].estatus + '.png';
                                     $innerDiv.appendChild($estatus);
 
                                     $div.appendChild($innerDiv);
@@ -57,28 +57,60 @@ if($getReporte){
 
                                     // crea un boton nuevo y agregalo al DIV
                                     let $boton = document.createElement('button');
-                                    $boton.setAttribute('id','btn-verReporte'); //CLASE PARA EL BOTON (reporte-boton)
+                                    $boton.setAttribute('id', 'btn-verReporte'); //CLASE PARA EL BOTON (reporte-boton)
                                     $boton.innerHTML = "Ver Reporte";
                                     $div.appendChild($boton);
+                                    
+                                    $idReporte = response.data[i].id;
+
+                                    // TODO: LISTENER
+                                    $boton.addEventListener('click', () => {
+                                          console.log("ENTRO AL ACTION");
+                                          const  data = new FormData();
+                                          data.append('id', $idReporte);
+                                          axios({
+                                                      method: 'POST',
+                                                      url: "/api/user/obtener-reporte.php",
+                                                      data,
+                                                      headers: {
+                                                            "Content-Type": "multipart/form-data"
+                                                      },
+                                                })
+                                                .then((response) => {
+                                                      if (response.status === 200) {
+                                                            console.log(response.data[i].id);
+                                                            // console.log(response.data);
+                                                            // marker.setLngLat([response.data.data.ubicacion]);
+                                                      }
+                                                })
+                                                .catch((error) => {
+                                                      if (error.response) {
+                                                            console.log(error.response);
+                                                            // alert(error.response.data.message);
+                                                            alert("No hay reportes");
+                                                      }
+                                                });
+                                    });
 
                                     let $hr = document.createElement('hr');
                                     $hr.classList.add('reporte-hr');
                                     $div.appendChild($hr);
 
-                              $contenedorDerecho.appendChild($div);
-                        }
+                                    $contenedorDerecho.appendChild($div);
+                              }
 
-                  } else {
-                        alert(response.data.message);
-                        
-                  }
-            })
-            .catch((error)=>{
-                  if (error.response){
-                        console.log(error.response);
-                        // alert(error.response.data.message);
-                        alert("No hay reportes");
-                  }
-            });
-        });
+                        } else {
+                              alert(response.data.message);
+
+                        }
+                  })
+                  .catch((error) => {
+                        if (error.response) {
+                              console.log(error.response);
+                              // alert(error.response.data.message);
+                              alert("No hay reportes");
+                        }
+                  });
+      });
 }
+
